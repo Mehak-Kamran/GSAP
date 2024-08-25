253b12ffe442e6255053c65f030852c52fefcee5
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-08-25 18:57:19 +0000

    yoyo,repeat,timeline etc properties

--- lec1/script.js
+++ lec1/script.js
@@ -1,20 +1,40 @@
-gsap.from("#box1",{
+var t1=gsap.timeline()
+
+
+
+t1.from("#box1",{
     x:1000,
     duration:2,
     delay:1,
     rotate:360,
+    yoyo:true,
     borderRadius:100,
     backgroundColor:"blue",
-    scale:2
+    scale:2,
+    repeat:-1
 })
 
-gsap.to("#box2",{
+t1.to("#box2",{
     x:1000,
+    repeat:-1,
+    yoyo:true,
     duration:2,
     delay:1,
     rotate:360,
     borderRadius:100,
     backgroundColor:"orange",
-    scale:0.7
+    scale:0.7,
+    
+})
+
+t1.to("h1",{
+    color:"red",
+    opacity:0,
+    duration:2,
+    delay:1,
+    y:200,
+    stagger:-1,
+    repeat:-1,
+    yoyo:true
 })
 
