bb539918ce22fee6bfbe9aac736f9b29ae89c486
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-08-25 19:40:52 +0000

    navbar

--- lec1/script.js
+++ lec1/script.js
@@ -2,39 +2,56 @@ var t1=gsap.timeline()
 
 
 
-t1.from("#box1",{
-    x:1000,
-    duration:2,
-    delay:1,
-    rotate:360,
-    yoyo:true,
-    borderRadius:100,
-    backgroundColor:"blue",
-    scale:2,
-    repeat:-1
-})
+// t1.from("#box1",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     yoyo:true,
+//     borderRadius:100,
+//     backgroundColor:"blue",
+//     scale:2,
+//     repeat:-1
+// })
 
-t1.to("#box2",{
-    x:1000,
-    repeat:-1,
-    yoyo:true,
-    duration:2,
-    delay:1,
-    rotate:360,
-    borderRadius:100,
-    backgroundColor:"orange",
-    scale:0.7,
+// t1.to("#box2",{
+//     x:1000,
+//     repeat:-1,
+//     yoyo:true,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     borderRadius:100,
+//     backgroundColor:"orange",
+//     scale:0.7,
     
+// })
+
+// t1.to("h1",{
+//     color:"red",
+//     opacity:0,
+//     duration:2,
+//     delay:1,
+//     y:200,
+//     stagger:-1,
+//     repeat:-1,
+//     yoyo:true
+// })
+
+t1.from("#heading1",{
+  y:-30,
+  duration:2,
+  delay:0.5,
+  opacity:0.1
 })
 
-t1.to("h1",{
-    color:"red",
-    opacity:0,
+
+
+t1.from("#heading",{
+    y:-30,
     duration:2,
-    delay:1,
-    y:200,
-    stagger:-1,
-    repeat:-1,
-    yoyo:true
-})
+    delay:0.2,
+    opacity:0.1,
+    stagger:1
+  })
 
