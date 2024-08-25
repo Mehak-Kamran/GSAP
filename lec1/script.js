56333bc34622ad9d33537c5aa3d7c6e0830f404e
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-08-25 13:40:10 +0000

    to and from box animations

---
+++ lec1/script.js
@@ -0,0 +1,20 @@
+gsap.from("#box1",{
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    borderRadius:100,
+    backgroundColor:"blue",
+    scale:2
+})
+
+gsap.to("#box2",{
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    borderRadius:100,
+    backgroundColor:"orange",
+    scale:0.7
+})
+
