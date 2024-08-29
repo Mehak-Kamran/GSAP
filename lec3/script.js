2d5e2a8a56d4a684e9a3ecf50cc5f5f54b0d56fe
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-08-29 18:49:54 +0000

    svg string

--- lec3/script.js
+++ lec3/script.js
@@ -1,2 +1,14 @@
-var i_path= "M 300 100 Q 1000 100 1000 100"
-var f_path="M 300 100 Q 1000 100 1000 100"
+var i_path= "M 60 100 Q 1000 100 1200 100"
+var f_path="M 60 100 Q 1000 100 1200 100"
+
+var string=document.querySelector("#string")
+string.addEventListener("mousemove",(dets)=>{
+    path=`M 60 100 Q 1000 ${dets.y} 1200 100`
+    })
+})
+
+var string=document.querySelector("#string")
+string.addEventListener("mouseleave",(event)=>{
+    console.log("bye")
+
+})
