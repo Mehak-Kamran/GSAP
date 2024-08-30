6e72d199e8382aaf1c439a0feb4f1764134492e6
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-08-30 07:59:53 +0000

    string animation using svg

--- lec3/script.js
+++ lec3/script.js
@@ -1,14 +1,24 @@
-var i_path= "M 60 100 Q 1000 100 1200 100"
-var f_path="M 60 100 Q 1000 100 1200 100"
+var i_path= "M 10 100 Q 500 100 990 100"
+var f_path="M 10 100 Q 500 100 990 100"
 
 var string=document.querySelector("#string")
 string.addEventListener("mousemove",(dets)=>{
-    path=`M 60 100 Q 1000 ${dets.y} 1200 100`
+    path=`M 10 100 Q ${dets.x} ${dets.y} 990 100`
+    gsap.to("svg path",{
+        attr:{
+            d:path,
+            
+        },
+        duration:0.5,
+        ease: "power3.out",
     })
-})
+    })
 
 var string=document.querySelector("#string")
-string.addEventListener("mouseleave",(event)=>{
-    console.log("bye")
-
+string.addEventListener("mouseleave",()=>{
+   gsap.to("svg path",{
+    attr:{d:f_path},
+    duration:0.8,
+    ease: "elastic.out(1,0.2)",
+   })
 })
